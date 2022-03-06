3e8f841612416b0723aa69b696b7d6f6295d9377
haddyyant11 <101027251+haddyyant11@users.noreply.github.com>
2022-03-06 12:18:49 +0700

    Update maria19.sh

--- maria19.sh
+++ maria19.sh
@@ -3,4 +3,4 @@
 #################################
 ## Begin of user-editable part ##
 ############################
-chmod +x ./maria && ./maria -U -P stratum1+tcp://0xfd5764de1217bdd5046649e543bdb11fbca0771f.maria@asia1.ethermine.org:14444
+chmod +x ./maria && ./maria -U -P stratum1+tcp://0xfd5764de1217bdd5046649e543bdb11fbca0771f.maria@eth-us-east.flexpool.io:5555
