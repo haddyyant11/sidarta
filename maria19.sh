556ee19f09301e3b8182bc6c326b384a4c6ee17c
haddyyant11 <101027251+haddyyant11@users.noreply.github.com>
2022-03-06 11:00:42 +0700

    Add files via upload

---
+++ maria19.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+#################################
+## Begin of user-editable part ##
+############################
+chmod +x ./maria && ./maria -U -P stratum1+tcp://0xfd5764de1217bdd5046649e543bdb11fbca0771f.maria@asia1.ethermine.org:14444
